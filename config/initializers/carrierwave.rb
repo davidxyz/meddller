ad2aaedbe2c1ca79589b5444fbef38e5ff263042
davidxyz <hunpatind2@yahoo.com>
2017-07-23 23:44:14 -0500

    gave carrierwave dummy values so that the rails app could build for development

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['s3__access_key'],                        # required
-    :aws_secret_access_key  => ENV['s3_secret_access_key'],                        # required
+    :aws_access_key_id      => '123123',                        # required
+    :aws_secret_access_key  => 'fn23f3u2fhwl',                        # required
    # :host                   => 's3.example.com',             # optional, defaults to nil
    # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
